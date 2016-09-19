9f0adc77ae4ab57ca7e7dc57ed3bc2b277e8a7c1
Lana Lang <nghipt@qsoft.com>
2016-09-19 19:09:45 +0700

    click row show form edit

--- resources/views/merchant/index.blade.php
+++ resources/views/merchant/index.blade.php
@@ -27,7 +27,7 @@
         </thead>
         <tbody>
         @foreach($data as $row)
-            <tr>
+            <tr onclick="merchant.edit({{$row->id}})">
                 <th class="text-right" scope="row">{{$row->name}}</th>
                 <td class="text-right">NULL</td>
                 <td class="text-right">NULL</td>
@@ -50,17 +50,17 @@
                 <td>{{$row->inferred_algo_name}}</td>
                 <td class="text-center">
                     @if ($row->status === 1)
-                        <button type="button" onclick="merchant.edit({{$row->id}})" class="btn btn-small btn-yellow btn-draft">Draft</button>
+                        <button type="button"  class="btn btn-small btn-yellow btn-draft">Draft</button>
                     @elseif ($row->status == 2)
-                        <button type="button" onclick="merchant.edit({{$row->id}})" class="btn btn-small btn-green btn-ready">Ready</button>
+                        <button type="button"  class="btn btn-small btn-green btn-ready">Ready</button>
                     @elseif ($row->status == 3)
-                        <button type="button" onclick="merchant.edit({{$row->id}})" class="btn btn-small btn-prodd">PROD-D</button>
+                        <button type="button"  class="btn btn-small btn-prodd">PROD-D</button>
                     @elseif ($row->status == 4)
-                        <button type="button" onclick="merchant.edit({{$row->id}})" class="btn btn-small btn-green btn-prodz">Prod-z</button>
+                        <button type="button"  class="btn btn-small btn-green btn-prodz">Prod-z</button>
                     @elseif ($row->status == 5)
-                        <button type="button" onclick="merchant.edit({{$row->id}})" class="btn btn-small btn-green btn-edit">Edit</button>
+                        <button type="button"  class="btn btn-small btn-green btn-edit">Edit</button>
                     @elseif ($row->status == 6)
-                        <button type="button" onclick="merchant.edit({{$row->id}})" class="btn btn-small btn-moss btn-update">Update</button>
+                        <button type="button" class="btn btn-small btn-moss btn-update">Update</button>
                     @endif
                 </td>
             </tr>
